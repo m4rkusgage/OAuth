bbc52e72f9254351dffab6e071c801cde9eac788
Markus Gage <markgage86@gmail.com>
2017-11-07 22:17:50 -0500

    initial project setup

---
+++ OAuth/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by Markus Gage on 2017-11-07.
+//  Copyright © 2017 Mark Gage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
